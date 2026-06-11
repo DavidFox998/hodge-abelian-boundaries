679a330e403b1623ccc360314b4ab2e533283c17
DavidFox998 <davidjfox998@gmail.com>
2026-06-11 02:19:49 -0700

    [Opera Numerorum] move imports before /-! docstring (Lean 4.12 fix)

--- lean/C04_Comparison.lean
+++ lean/C04_Comparison.lean
@@ -1,9 +1,9 @@
+import Mathlib.Data.Nat.Basic
 /-!
 # C04 -- Comparison Isomorphisms
 Clay Wall 3 | Opera Numerorum | David Fox | June 2026
 -- clay := true | sorry_count := 0 | status := FOUNDATIONAL
 -/
-import Mathlib.Data.Nat.Basic
 
 namespace HodgeAbelian
 
